3bb761e57003d962bcab288017f11918f8c4c0d1
merfankarami <mohammaderfankarami567@gmail.com>
2021-04-23 22:39:02 +0430

    react router implemented

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Mouse from "./Components/MouseIcon/Mouse";
 import ElevateAppBar from "./Components/AppBar/Bar";
 import Title from "./Components/Typography/Typograhpy";
 import About from "./Components/AboutMe/About";
+import { Link, Route, Switch } from "react-router-dom";
 import pic from "./Media/negative-space.jpg";
 
 import "./App.css";
@@ -10,39 +11,37 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <div className="bgContainer">
-        <img src={pic} alt="negative-space" />
-      </div>
-      <div className="Container">
-        <div className="intro">
-          <div className="intro-sub">I am Mohammad Erfan Karami</div>
-          <h1>
-            Creative <span> Designer</span>
-          </h1>
-          <div className="intro-description">
-            I am a fully professional freelance creative User Interface Designer
-            & Developer Involving with latest web designing and technologies is
-            a great feel free to contact creative.
-          </div>
-          <div className="social-icons">
-            <ul className="list-inline">
-              <div>
-                <Li href="#">mail_outline</Li>
-              </div>
-              <div>
-                <Li href="#">link</Li>
-              </div>
-              <div>
-                <Li href="#">phone</Li>
-              </div>
-            </ul>
-          </div>
+      <Route path="/">
+        <div className="bgContainer">
+          <img src={pic} alt="negative-space" />
         </div>
-        <Mouse />
-      </div>
+        <div className="Container">
+          <div className="intro">
+            <div className="intro-sub">I am Mohammad Erfan Karami</div>
+            <h1>
+              Creative <span> Designer</span>
+            </h1>
+            <div className="intro-description">
+              I am a fully professional freelance creative User Interface
+              Designer & Developer Involving with latest web designing and
+              technologies is a great feel free to contact creative.
+            </div>
+            <div className="social-icons">
+              <ul className="list-inline">
+                <Li href="#">mail_outline</Li>
+                <Li href="#">link</Li>
+                <Li href="#">phone</Li>
+              </ul>
+            </div>
+          </div>
+          <Mouse />
+        </div>
+      </Route>
       <ElevateAppBar />
       <Title>About Me</Title>
-      <About />
+      <Route path="/about">
+        <About />
+      </Route>
     </div>
   );
 }
--- src/Components/AppBar/Bar.js
+++ src/Components/AppBar/Bar.js
@@ -3,8 +3,15 @@ import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import Button from '@material-ui/core/Button';
+import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
+
+import { Link, Route, Switch } from "react-router-dom";
+
+import PropTypes from "prop-types";
+import useScrollTrigger from "@material-ui/core/useScrollTrigger";
+import Box from "@material-ui/core/Box";
+import Container from "@material-ui/core/Container";
 
 const useStyles = makeStyles({
   appBar: {
@@ -15,38 +22,61 @@ const useStyles = makeStyles({
     backgroundColor: "#ffffff",
   },
   typography: {
-      color: "#68c3a3",
-      fontSize: 26,
-      fontWeight: "bold",
+    color: "#68c3a3",
+    fontSize: 26,
+    fontWeight: "bold",
   },
   divButton: {
     position: "absolute",
-    right: 90  
+    right: 90,
   },
   button: {
     fontSize: 18,
-    fontWeight: "bold"
-  }
+    fontWeight: "bold",
+    "&:hover": {
+      color: "#68c3a3"
+    }
+  },
 });
+
+function ElevationScroll(props) {
+  const { children, window } = props;
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+    target: window ? window() : undefined,
+  });
+
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0,
+  });
+}
 
 export default function ElevateAppBar(props) {
   const classes = useStyles();
   return (
     <React.Fragment>
       <CssBaseline />
-      <AppBar className={classes.appBar}>
-        <Toolbar>
-          <Typography  className={classes.typography}><span style={{color: "#163550"}}>I AM</span> MOHAMMAD ERFAN KARAMI</Typography>
-          <div className={classes.divButton}>
-          <Button className={classes.button}>HOME</Button>
-          <Button className={classes.button}>ABOUT</Button>
-          <Button className={classes.button}>RESUME</Button>
-          <Button className={classes.button}>SKILLS</Button>
-          <Button className={classes.button}>CONTACT</Button>
-          </div>
-        </Toolbar>
-      </AppBar>
-      <Toolbar />
+      <ElevationScroll {...props}>
+        <AppBar className={classes.appBar}>
+          <Toolbar>
+            <Typography className={classes.typography}>
+              <span style={{ color: "#163550" }}>I AM</span> MOHAMMAD ERFAN
+              KARAMI
+            </Typography>
+            <div className={classes.divButton}>
+              <Link to="/"><Button className={classes.button}>HOME</Button></Link>
+              <Link to="/about"><Button className={classes.button}>ABOUT</Button></Link>
+              <Button className={classes.button}>RESUME</Button>
+              <Button className={classes.button}>SKILLS</Button>
+              <Button className={classes.button}>CONTACT</Button>
+            </div>
+          </Toolbar>
+        </AppBar>
+      </ElevationScroll>
     </React.Fragment>
   );
 }
